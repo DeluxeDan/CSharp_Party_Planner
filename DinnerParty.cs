b228279d545003b5479240f7fdd6856e2eba7436
DeluxeDan <51492363+DeluxeDan@users.noreply.github.com>
2020-06-10 15:52:40 -0400

    Add files via upload

---
+++ DinnerParty.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Party_Planner
+{
+    class DinnerParty
+    {
+        const int CostOfFoodPerPerson = 25;
+        private int numberOfPeople;  //a private variable can only be read and written by that instance - or 
+                                     //another instance of DinnerParty. Other objects won't know it's there.
+        public decimal CostOfBeveragesPerPerson;
+        public decimal CostOfDecorations = 0;
+
+        public void SetPartyOptions(int people, bool fancy)
+        {
+            numberOfPeople = people;
+            CalculateCostOfDecorations(fancy);
+        }
+
+        public int GetNumberOfPeople()
+        {
+            return numberOfPeople;
+        }
+        public void SetHealthyOption(bool healthyOption)
+        {
+            if (healthyOption)
+            {
+                CostOfBeveragesPerPerson = 5.0M; // M is used for literal monetary values. Think M for money.
+            }
+            else
+            {
+                CostOfBeveragesPerPerson = 20.0M;
+            }
+        }
+
+        public void CalculateCostOfDecorations(bool fancyDecor)
+        {
+            if (fancyDecor)
+            {
+                CostOfDecorations = (numberOfPeople * 15.00M) + 50M;
+            }
+            else
+            {
+                CostOfDecorations = (numberOfPeople * 7.50M) + 30M;
+            }
+        }
+
+        public decimal CalculateCost(bool healthyOption)
+        {
+            decimal totalCost = CostOfDecorations + ((CostOfBeveragesPerPerson + CostOfFoodPerPerson) * numberOfPeople);
+
+            if (healthyOption)
+            {
+                return totalCost * .95M; //apply the 5% discount to the overall event cost if the non-alcoholic option was chosen
+            }
+            else
+            {
+                return totalCost;
+            }
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Party_Planner
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculateCost(checkBox2.Checked); //healthy option is checked.
+            labelCost.Text = Cost.ToString("c"); //passing c tells it to use local currency value. Can also use f3
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinnerParty = new DinnerParty(); // { NumberOfPeople = 5 };
+            dinnerParty.SetPartyOptions(5, checkBox1.Checked);
+            dinnerParty.SetHealthyOption(checkBox2.Checked);
+            //dinnerParty.CalculateCostOfDecorations(checkBox1.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //dinnerParty.numberOfPeople = (int)numericUpDown1.Value; //casting numericUpdown1.value because it is a decimal
+            dinnerParty.SetPartyOptions((int)numericUpDown1.Value, checkBox1.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations(checkBox1.Checked);
+            DisplayDinnerPartyCost(); //recalculates and displays new dinner party cost.
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetHealthyOption(checkBox2.Checked);
+            DisplayDinnerPartyCost();
+        }
+    }
+}
